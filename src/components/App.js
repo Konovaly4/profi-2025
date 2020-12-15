2aba7d35dcb37b48f515dc74ad72c126ac4099d6
Admin <maxkonovalov82@yandex.ru>
2020-12-15 17:02:00 +0300

    temporary test block added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import TestBlock from './TestBlock/TestBlock';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <TestBlock />
     </div>
   );
 }
