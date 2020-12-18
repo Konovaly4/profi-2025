2d7a388e522d104fd5f80947fb63830a688200b4
Admin <maxkonovalov82@yandex.ru>
2020-12-18 17:51:42 +0300

    change of button html

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import './Button.css';
 const Button = (props) => {
 
   return (
-    <button className='button'>{props.placeholder} onClick={props.onPress}</button>
+    <button className='button' onClick={props.onPress}>{props.placeholder}</button>
   )
 }
 
